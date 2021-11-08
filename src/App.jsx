7126d9e1f925f121d9751c8512aaeea99d11975e
HappyKOTE <vpavlov.moscow@gmail.com>
2021-11-08 18:37:24 +0300

    some fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
         {
           'authorName': 'bot',
           'message': 'Приветствую тебя, человек!',
-          'timestamp': '2021-11-06'
+          'timestamp': ['07.11.2021', '12:00:00']
         }
       ]
     },
@@ -21,33 +21,61 @@ function App() {
         {
           'authorName': 'Миловидов',
           'message': '(говорит мягким басом, плавно, важно, авторитетно) Итак, нашего Ивана Семеныча уже не существует!.. Все, что было  у  него приятного, исчезло вместе с ним!',
-          'timestamp': '2021-11-06'
+          'timestamp': ['07.11.2021', '12:00:00']
         },
         {
           'authorName': 'Кутило-Завалдайский',
           'message': '(со вздохом) Сколько у него было душ и десятин пахотной земли?',
-          'timestamp': '2021-11-07'
+          'timestamp': ['07.11.2021', '13:00:00']
         },
         {
           'authorName': 'Миловидов',
           'message': 'Главное его имение,  село  Курохвостово,  не  помню:  Астраханской  или Архангельской губернии? Душ  по  последней  ревизии  числилось  пятьсот;  по крайней мере, так выразился, говоря со мною, заседатель  гражданской  палаты Фирдин, Иван Петрович.',
-          'timestamp': '2021-11-07'
+          'timestamp': ['07.11.2021', '14:00:00']
         },
         {
           'authorName': 'Кн. Батог-Батыев',
           'message': '(с подвязанною щекою; говорит шепелявя и с присвистом) Фирдин?..  Какой  Фирдин?  Не  тот  ли,  который  ранен  был  на  дуэли ротмистром Кавтыревым?',
-          'timestamp': '2021-11-07'
+          'timestamp': ['07.11.2021', '15:00:00']
         },
         {
           'authorName': 'user',
           'message': 'Вы чего несёте?',
-          'timestamp': '2021-11-07'
+          'timestamp': ['07.11.2021', '16:00:00']
         }
       ]
     }
   ])
   const [activeChat, setActiveChat] = useState(0)
   const [message, setMessage] = useState('')
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  }
+  const saveMessage = () => {
+    const now = [
+      new Date().toLocaleDateString(),
+      new Date().toLocaleTimeString()
+    ]
+    const object = {
+      'authorName': 'user',
+      'message': message,
+      'timestamp': now
+    }
+    chats[activeChat].chatLog.push(object)
+    setMessage('')
+  }
+  const dateAndTime = (value) => {
+    const now = new Date().toLocaleDateString()
+    if (value[0] === now) {
+      return value[1]
+    } else {
+      return value[0]
+    }
+  }
+
+  useEffect(() => {
+    console.log('change')
+  }, [chats])
 
   return (
     <div className="row">
@@ -62,7 +90,7 @@ function App() {
             <div className="position-relative">
               <div className="d-flex justify-content-between">
                 <div className="fw-bold text-truncate">{value.chatName}</div>
-                <div className="text-muted">{value.chatLog[value.chatLog.length-1].timestamp}</div>
+                <div className="text-muted">{dateAndTime(value.chatLog[value.chatLog.length-1].timestamp)}</div>
               </div>
               <div className="text-muted text-truncate">{value.chatLog[value.chatLog.length-1].message}</div>
               <div className="position-absolute top-50 start-0 translate-middle-y bg-secondary rounded-circle" style={{ backgroundImage: 'url(' + value.backgroundImage + ')'  }}></div>
@@ -87,14 +115,13 @@ function App() {
           <div className="h-100 p-3 overflow-auto">
 
           {chats[activeChat].chatLog.map((value, idx) => (
-            <div
-              className={(value.authorName === 'user') ? 'p-3 mb-2 alert alert-primary d-inline-block' : 'p-3 mb-2 alert alert-secondary d-inline-block'}
-              key={idx}
-            >
-              <div className="d-flex justify-content-between">
-                <div className="fw-bold me-3">{value.authorName}</div>
-                <div>{value.message}</div>
-                <div className="text-muted ms-3">{value.timestamp}</div>
+            <div key={idx}>
+              <div className={(value.authorName === 'user') ? 'p-3 mb-2 alert alert-primary d-inline-block' : 'p-3 mb-2 alert alert-secondary d-inline-block'}>
+                <div className="d-flex justify-content-between">
+                  <div className="fw-bold me-3">{value.authorName}</div>
+                  <div>{value.message}</div>
+                  <div className="text-muted ms-3">{dateAndTime(value.timestamp)}</div>
+                </div>
               </div>
             </div>
           ))}
@@ -103,8 +130,8 @@ function App() {
           <div className="border-top p-3 bg-light">
             <form className="m-0 p-0">
               <div className="input-group">
-                <input type="text" className="form-control border-0 rounded bg-transparent" placeholder="написать сообщение" value={message}></input>
-                <Button variant="primary" className="rounded-pill border-0 ms-2"><i className="bi bi-send"></i></Button>
+                <input type="text" className="form-control border-0 rounded bg-transparent" placeholder="написать сообщение" autoFocus value={message} onChange={handleChange}></input>
+                <Button variant="primary" className="rounded-pill border-0 ms-2" onClick={saveMessage}><i className="bi bi-send"></i></Button>
               </div>
             </form>
           </div>
